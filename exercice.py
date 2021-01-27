af23879f1bda48efefb403ba754e125c3f08f2b0
Sooju <77995929+Sooju@users.noreply.github.com>
2021-01-26 22:19:40 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -9,11 +9,11 @@ def square_root(a: float) -> float:
 
 
 def square(a: float) -> float:
-    return 0.0
+    return square (a)
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return average (a)
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
